c9b12aa1baecda46012766c82d4432bcaee6aaa7
Tomasz Nowicki <tn@semihalf.com>
2016-06-14 13:59:54 +0200

    ACPI/PCI: Match PCI config space accessors against platfrom specific ECAM quirks
    Some platforms may not be fully compliant with generic set of PCI config
    accessors. For these cases we implement the way to overwrite accessors
    set. Algorithm traverses available quirk list, matches against
    <oem_id, oem_table_id, oem_rev, domain, bus number> tuple and returns
    corresponding PCI config ops. oem_id, oem_table_id and oem_rev come from
    MCFG table standard header. All quirks can be defined using
    DECLARE_ACPI_MCFG_FIXUP() macro and kept self contained. Example:
    
    /* Custom PCI config ops */
    static struct pci_generic_ecam_ops foo_pci_ops = {
    	.bus_shift	= 24,
    	.pci_ops = {
    		.map_bus = pci_ecam_map_bus,
    		.read = foo_ecam_config_read,
    		.write = foo_ecam_config_write,
    	}
    };
    
    DECLARE_ACPI_MCFG_FIXUP(&foo_pci_ops, <oem_id_str>, <oem_table_id_str>,
                            <oem_rev>, <domain_nr>, <bus_nr>);
    
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>

--- drivers/acpi/pci_mcfg.c
+++ drivers/acpi/pci_mcfg.c
@@ -22,6 +22,7 @@
 #include <linux/kernel.h>
 #include <linux/pci.h>
 #include <linux/pci-acpi.h>
+#include <linux/pci-ecam.h>
 
 /* Structure to hold entries from the MCFG table */
 struct mcfg_entry {
@@ -34,6 +35,43 @@ struct mcfg_entry {
 
 /* List to save MCFG entries */
 static LIST_HEAD(pci_mcfg_list);
+
+extern struct pci_cfg_fixup __start_acpi_mcfg_fixups[];
+extern struct pci_cfg_fixup __end_acpi_mcfg_fixups[];
+
+struct pci_ecam_ops *pci_mcfg_get_ops(int domain, int bus_num)
+{
+	struct pci_cfg_fixup *f;
+	struct acpi_table_header *mcfg_header;
+	struct acpi_table_mcfg *mcfg_table;
+	acpi_status status;
+
+	status = acpi_get_table(ACPI_SIG_MCFG, 0, &mcfg_header);
+	if (ACPI_FAILURE(status))
+		return &pci_generic_ecam_ops;
+
+	/*
+	 * Match against platform specific quirks and return corresponding
+	 * CAM ops.
+	 *
+	 * First match against PCI topology <domain:bus> then use OEM ID and
+	 * OEM revision from MCFG table standard header.
+	 */
+	mcfg_table = (struct acpi_table_mcfg *)mcfg_header;
+	for (f = __start_acpi_mcfg_fixups; f < __end_acpi_mcfg_fixups; f++) {
+		if ((f->domain == domain || f->domain == PCI_MCFG_DOMAIN_ANY) &&
+		    (f->bus_num == bus_num || f->bus_num == PCI_MCFG_BUS_ANY) &&
+		    (!strncmp(f->oem_id, mcfg_table->header.oem_id,
+			      ACPI_OEM_ID_SIZE)) &&
+		    (!strncmp(f->oem_table_id, mcfg_table->header.oem_table_id,
+			      min_t(size_t, strlen(f->oem_id),
+				    ACPI_OEM_TABLE_ID_SIZE))) &&
+		    (f->oem_revision == mcfg_table->header.oem_revision))
+			return f->ops;
+	}
+	/* No quirks, use ECAM */
+	return &pci_generic_ecam_ops;
+}
 
 phys_addr_t pci_mcfg_lookup(u16 seg, struct resource *bus_res)
 {
