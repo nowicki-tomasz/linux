237df520f9ffd4bfa5bfcf74c5dfe2e45a8e747c
Marc Zyngier <maz@kernel.org>
2019-01-04 22:22:30 +0100

    KVM: arm64: nv: Support multiple nested Stage-2 mmu structures
    Add Stage-2 mmu data structures for virtual EL2 and for nested guests.
    We don't yet populate shadow Stage-2 page tables, but we now have a
    framework for getting to a shadow Stage-2 pgd.
    
    We allocate twice the number of vcpus as Stage-2 mmu structures because
    that's sufficient for each vcpu running two translation regimes without
    having to flush the Stage-2 page tables.
    
    Co-developed-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -10,6 +10,13 @@ static inline bool nested_virt_in_use(const struct kvm_vcpu *vcpu)
 		test_bit(KVM_ARM_VCPU_HAS_EL2, vcpu->arch.features);
 }
 
+extern void kvm_init_nested(struct kvm *kvm);
+extern int kvm_vcpu_init_nested(struct kvm_vcpu *vcpu);
+extern void kvm_init_s2_mmu(struct kvm_s2_mmu *mmu);
+extern struct kvm_s2_mmu *lookup_s2_mmu(struct kvm *kvm, u64 vttbr, u64 hcr);
+extern void kvm_vcpu_load_hw_mmu(struct kvm_vcpu *vcpu);
+extern void kvm_vcpu_put_hw_mmu(struct kvm_vcpu *vcpu);
+
 int handle_wfx_nested(struct kvm_vcpu *vcpu, bool is_wfe);
 extern bool __forward_traps(struct kvm_vcpu *vcpu, unsigned int reg,
 			    u64 control_bit);
