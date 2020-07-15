1f5d6ac372d854dcee31936d29838625f80d0da0
Tomasz Nowicki <tnowicki@google.com>
2020-07-15 16:18:34 +0200

    vfio: platform: Wire up clock notification
    Register callback for each VFIO clock in order to notify guest about
    any relevant clock reconfiguration.
    
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>
    Change-Id: I958a127d37b5fd92ccb504b60e5b521a96820e28

--- drivers/vfio/platform/vfio_platform_clk.c
+++ drivers/vfio/platform/vfio_platform_clk.c
@@ -8,11 +8,46 @@
 
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
+#include <linux/notifier.h>
 #include <linux/slab.h>
 #include <linux/types.h>
 #include <linux/vfio.h>
+#include <linux/vhost_vfio.h>
 
 #include "vfio_platform_private.h"
+
+static int vfio_platform_clk_notifier_cb(struct notifier_block *nb,
+					 unsigned long event, void *data)
+{
+	struct vfio_platform_device *pvdev = container_of(nb,
+						struct vfio_platform_device,
+						clk_nb);
+	struct clk_devres *clk_res = &pvdev->clk_res;
+	struct clk_notifier_data *cnd = data;
+	struct virtio_vfio_clk_event evt = {
+			.msg = event,
+			.old_rate = cnd->old_rate,
+			.new_rate = cnd->new_rate,
+	};
+	struct vhost_dev *vhost;
+	int index, found = 0;
+
+	for (index = 0; index < clk_res->num_clks; index++) {
+		if (clk_res->clk_bulk[index].clk == cnd->clk) {
+			found = 1;
+			break;
+		}
+	}
+
+	if (WARN_ON(found == 0))
+		return NOTIFY_BAD;
+
+	vhost = clk_res->vdev[index];
+	if (!vhost)
+		return NOTIFY_OK;
+
+	return vhost_pipe_send_evt(vhost, &evt, sizeof(evt));
+}
 
 int vfio_platform_clk_init(struct vfio_platform_device *vdev)
 {
@@ -35,6 +70,8 @@ int vfio_platform_clk_init(struct vfio_platform_device *vdev)
 	if (!clk_res->vdev)
 		return -ENOMEM;
 
+	vdev->clk_nb.notifier_call = vfio_platform_clk_notifier_cb;
+
 	return 0;
 }
 
@@ -42,6 +79,16 @@ static int vfio_platform_clk_add(struct vfio_platform_device *vdev,
 				 struct vhost_dev *vhost, int index)
 {
 	struct clk_devres *clk_res = &vdev->clk_res;
+	struct device *dev = vdev->device;
+	int ret;
+
+	ret = clk_notifier_register(clk_res->clk_bulk[index].clk,
+				    &vdev->clk_nb);
+	if (ret) {
+		dev_err(dev, "failed to register clock notifier (err = %d)\n",
+			ret);
+		return ret;
+	}
 
 	clk_res->vdev[index] = vhost;
 	return 0;
@@ -52,6 +99,7 @@ static int vfio_platform_clk_del(struct vfio_platform_device *vdev,
 {
 	struct clk_devres *clk_res = &vdev->clk_res;
 
+	clk_notifier_unregister(clk_res->clk_bulk[index].clk, &vdev->clk_nb);
 	clk_res->vdev[index] = NULL;
 	return 0;
 }
@@ -75,4 +123,12 @@ int vfio_platform_clk_register_vhost(struct vfio_platform_device *vdev,
 
 void vfio_platform_clk_cleanup(struct vfio_platform_device *vdev)
 {
+	struct clk_devres *clk_res = &vdev->clk_res;
+	int i;
+
+	for (i = 0; i < vdev->clk_res.num_clks; i++) {
+		clk_notifier_unregister(clk_res->clk_bulk[i].clk,
+					&vdev->clk_nb);
+		clk_res->vdev[i] = NULL;
+	}
 }
