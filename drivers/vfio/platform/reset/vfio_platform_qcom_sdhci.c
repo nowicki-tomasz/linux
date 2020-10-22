2e768dd1d0e028942b9e2f4abfb7ec9d29515e2b
Tomasz Nowicki <tnowicki@google.com>
2020-10-22 19:05:16 +0000

    trogdor: vfio: sdhci: add support for SDHCI reset hook
    Signed-off-by: Tomasz Nowicki <tn@semihalf.com>
    Change-Id: I9a87951fd2249808799be125904f838cfa06e6c6

---
+++ drivers/vfio/platform/reset/vfio_platform_qcom_sdhci.c
@@ -0,0 +1,46 @@
+/*
+ * VFIO platform driver specialized for XHCI reset
+ * reset code is inherited from XHCI native driver
+ *
+ * Copyright 2016 Marvell Semiconductors, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/device.h>
+#include <linux/of.h>
+#include <linux/phy/phy.h>
+#include <linux/usb/phy.h>
+#include <linux/clk.h>
+
+#include "../vfio_platform_private.h"
+
+#define DRIVER_VERSION  "0.1"
+#define DRIVER_AUTHOR   "Tomasz Nowicki <tn@semihalf.com>"
+#define DRIVER_DESC     "Reset support for Qualcomm SDHCI vfio platform device"
+
+int vfio_platform_qcom_sdhci_reset(struct vfio_platform_device *vdev)
+{
+	return 0;
+}
+
+module_vfio_reset_handler("qcom,sc7180-sdhci", vfio_platform_qcom_sdhci_reset);
+
+MODULE_VERSION(DRIVER_VERSION);
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
